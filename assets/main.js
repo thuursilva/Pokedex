a74bdbcde251e45ae53c7f67cc23a54b61c9103b
Arthur Fernando <55673628+thuursilva@users.noreply.github.com>
2024-01-04 17:50:04 -0300

    Atualizações

--- assets/main.js
+++ assets/main.js
@@ -3,11 +3,34 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-//minimizando código
+function convertPokemon (pokemon){
+    return `
+    <li class="card">
+
+    <span class="number">#001</span>
+    <span class="name">${pokemon.name}</span>
+
+    <div class="detail">
+
+        <ol class="types">
+            <li class="type">Grass</li>
+            <li class="type">Poison</li>
+        </ol>
+
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+    </div>
+    </li>`
+}
+
 fetch(url)
-    //utilizando arrow function para retornar o response no formato json
     .then((response) => response.json())
-    //printando o retorno do primeiro then da promise no console
-    .then((responseBody) => console.log(responseBody))
-    //utilizando arrow function para coletar possível erro tbm
+    .then((responseBody) => responseBody.results)
+    .then((pokemonList) => {
+        
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemonList[i];
+            console.log(convertPokemon(pokemon))
+        }
+    })
     .catch((error) => console.log(error))
+
