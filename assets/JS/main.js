cc6f1e981dbe63d3a50a2ffe3d97c51385bac75e
Arthur Fernando <55673628+thuursilva@users.noreply.github.com>
2024-02-14 11:49:37 -0300

    Implementando a paginação em JS

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,28 +1,43 @@
-//inserindo os background dinamicamente pelo tipo
-function convertPokemon (pokemon){
-    return `
-    <li class="card ${pokemon.type}">
-
-    <span class="number">#${pokemon.number}</span>
-    <span class="name">${pokemon.name}</span>
-
-    <div class="detail">
-
-        <ol class="types">
-            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-        </ol>
-
-        <img src="${pokemon.photo}" alt="${pokemon.name}">
-    </div>
-    </li>`
-}
 
 const pokemonList = document.getElementById('pokemonlist')
+const loadButton = document.getElementById('loadMore')
+const limit = 5
+let offset = 0
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    const newHtml = pokemons.map(convertPokemon).join('')
-    pokemonList.innerHTML = newHtml
+//criando uma função para concatenar pokemons ao html já criado ao invés de substituir ao seleiconar "Load More"
+//retirando a função de conversão e inserindo no map já existente
+function loadPokemons(offset, limit){
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+        <li class="card ${pokemon.type}">
+    
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+    
+        <div class="detail">
+    
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+    
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
+        </div>
+        </li> 
+        `).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+//chamando a função
+loadPokemons(offset, limit)
+
+//adicionando ação a ser efetuada a clicar no botão
+//iremos incrementar em offset o valor de limit para carregar novos pokemon
+loadButton.addEventListener('click', () =>{
+    offset += limit
+    loadPokemons(offset, limit)
 })
+
 
 
 
